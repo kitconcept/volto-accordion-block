8cf7c755c6409028818c9c80276ba6da8b74ba73
Tishasoumya-02 <tishasoumya@gmail.com>
2024-11-14 13:51:47 +0530

    fix: prettier

--- packages/volto-accordion-block/src/index.js
+++ packages/volto-accordion-block/src/index.js
@@ -4,7 +4,7 @@ import View from './components/View';
 import layoutSchema from './components/LayoutSchema';
 import { AccordionStylingSchema } from './components/schema';
 import PanelsWidget from './components/Widgets/PanelsWidget';
- //import filterSVG from '@plone/volto/icons/filter.svg';
+//import filterSVG from '@plone/volto/icons/filter.svg';
 // import clearSVG from '@plone/volto/icons/clear.svg';
 import { defineMessages, createIntlCache, createIntl } from 'react-intl';
 
@@ -55,6 +55,7 @@ const extendedSchema = (config) => {
 };
 
 const applyConfig = (config) => {
+  console.log(config.blocks.blocksConfig);
   config.widgets.type.panels = PanelsWidget;
   config.blocks.blocksConfig.accordion = {
     id: 'accordion',
@@ -67,7 +68,7 @@ const applyConfig = (config) => {
     mostUsed: false,
     blockHasOwnFocusManagement: true,
     sidebarTab: 1,
-    non_exclusive:true,
+    non_exclusive: true,
     schema: extendedSchema(config),
     schemaEnhancer: AccordionStylingSchema,
     // See https://react.semantic-ui.com/modules/accordion/
